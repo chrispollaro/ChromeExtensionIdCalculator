17feee383fe9a3489d6019933f7810f09b43cb1e
Christopher Pollaro <chris@chrispollaro.com>
2019-10-14 06:49:45 -0400

    Initial Utility setup

--- ChromeExtensionIdUtility/MainWindow.xaml.cs
+++ ChromeExtensionIdUtility/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+
+using ChromeExtensionsIdCalculator;
 
 namespace ChromeExtensionIdUtility
 {
@@ -23,6 +27,21 @@ namespace ChromeExtensionIdUtility
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void PEMBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                PEMLocation.Content = openFileDialog.FileName;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var extCalculator = new ExtensionIdentifier();
+            string path = PEMLocation.Content as string;
+            var id = extCalculator.CalculateId(path);
+            ExtensionId.Text = id;
         }
     }
 }
